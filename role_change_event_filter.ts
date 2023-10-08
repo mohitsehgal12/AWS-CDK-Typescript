ea5d1c63504733d59f4809ec935697717cfc6458
Mohit Sehgal <Sehgalrkss@gmail.com>
2023-10-08 10:42:32 +0530

    Update role_change_event_filter.ts

--- role_change_event_filter.ts
+++ role_change_event_filter.ts
@@ -1,8 +1,14 @@
+// Declare and define roleArns with actual role ARNs
+const roleArns: string[] = [
+  'arn:aws:iam::account-id-with-access:role/RoleWithAccess',
+  // Add more role ARNs as needed
+];
+
 const iamRoleChangeEventFilter = FilterPattern.all(
   // Assuming the "assume role" policy change
   FilterPattern.stringValue('$.eventName', '=', 'UpdateAssumeRolePolicy'),
   // Filter only on role ARNs with access to the snapshot bucket
   FilterPattern.any(
-    ...roleArns.map(roleArn => FilterPattern.stringValue('$.requestParameters.roleName', '=', roleArn))
+    ...roleArns.map((roleArn: string) => FilterPattern.stringValue('$.requestParameters.roleName', '=', roleArn))
   )
 );
