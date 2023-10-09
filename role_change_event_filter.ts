16c7dfac6740c1a08e1b46bef5126f4f57602cb6
Mohit Sehgal <Sehgalrkss@gmail.com>
2023-10-09 20:27:43 +0530

    Update role_change_event_filter.ts

--- role_change_event_filter.ts
+++ role_change_event_filter.ts
@@ -1,14 +1,14 @@
-// Declare and define roleArns with actual role ARNs
-const roleArns: string[] = [
-  'arn:aws:iam::account-id-with-access:role/RoleWithAccess',
-  // Add more role ARNs as needed
-];
+import { ArnPrincipal } from 'aws-cdk-lib/aws-iam';
+import { FilterPattern } from 'aws-cdk-lib/aws-logs';
+
+// Assuming stageProps is available in this file
+const roleArns: ArnPrincipal[] = [new ArnPrincipal(`arn:aws:iam::${stageProps.account}:role/ZoltarScanTaskRole`)];
 
 const iamRoleChangeEventFilter = FilterPattern.all(
   // Assuming the "assume role" policy change
   FilterPattern.stringValue('$.eventName', '=', 'UpdateAssumeRolePolicy'),
   // Filter only on role ARNs with access to the snapshot bucket
   FilterPattern.any(
-    ...roleArns.map((roleArn: string) => FilterPattern.stringValue('$.requestParameters.roleName', '=', roleArn))
+    ...roleArns.map((arnPrincipal) => FilterPattern.stringEquals('$.userIdentity.arn', arnPrincipal.arn))
   )
 );
